afca2b48178a77e927d154e1da8d30eacb92b2fa
Oleg Keri <okerixx@gmail.com>
2023-09-21 18:16:07 +0200

    move to stl utf8 convertion

--- src/utf8.cc
+++ src/utf8.cc
@@ -1,36 +1,29 @@
 #include <cstring>
-
-#include <unistr.h>
+#include <codecvt>
+#include <locale>
 
 #include "utf8.hh"
+
+namespace {
+static std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
+}  // namespace
 
 namespace utf8 {
 
 std::wstring convert(const char* str, size_t len) {
-    auto result = std::wstring();
-    result.resize(len + 1, L'\0');
-    u8_to_u32(reinterpret_cast<const uint8_t*>(str), len++,
-        reinterpret_cast<uint32_t*>(result.data()), &len);
-    result.resize(len);
-    return result;
+    return converter.from_bytes(str, str + len);
 }
 
 std::wstring convert(const char* str) {
-    return convert(str, strlen(str));
+    return converter.from_bytes(str);
 }
 
 std::wstring convert(const std::string& str) {
-    return convert(str.c_str(), str.length());
+    return converter.from_bytes(str);
 }
 
 std::string convert(const std::wstring& str) {
-    auto result = std::string();
-    size_t len = str.length() << 2;
-    result.resize(len, '\0');
-    u32_to_u8(reinterpret_cast<const uint32_t*>(str.c_str()), str.length(),
-        reinterpret_cast<uint8_t*>(result.data()), &len);
-    result.resize(len);
-    return result;
+    return converter.to_bytes(str);
 }
 
 }  // namespace utf8
