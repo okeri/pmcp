2b7073d0b35d93fcec92403f1cbee7af7ec7d9d6
Oleg Keri <okerixx@gmail.com>
2024-03-22 18:38:01 +0100

    add constructor for AtomicFifo::Node

--- src/AtomicLifo.hh
+++ src/AtomicLifo.hh
@@ -8,15 +8,17 @@ class AtomicLifo {
     struct Node {
         Data data;
         Node* next{nullptr};
+        explicit Node(Data&& newData) : data(std::move(newData)) {
+        }
     };
 
     std::atomic<Node*> head_{nullptr};
 
   public:
     void push(Data&& data) {
-        auto* node = new Node;  // NOLINT
-        node->data = std::move(data);
-        node->next = head_.load(std::memory_order::acquire);
+        // NOLINTNEXTLINE(cppcoreguidelines-owning-memory)
+        auto* node = new Node(std::move(data));
+        node->next = head_.load();
         while (!head_.compare_exchange_weak(node->next, node)) {
         }
     }
@@ -26,10 +28,10 @@ class AtomicLifo {
             while (!head_.compare_exchange_weak(head, head->next)) {
             }
             auto data = std::move(head->data);
-            delete head;  // NOLINT
+            delete head;  // NOLINT(cppcoreguidelines-owning-memory)
             return data;
         }
-        return {};
+       return {};
     }
 
     void waitNonEmpty() noexcept {
